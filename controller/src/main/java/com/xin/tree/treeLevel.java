a3de72af015501fdd32279d8e3db5770011ee139
xinjiakun <1913829181@qq.com>
2019-10-22 22:18:58 +0800

    commit

---
+++ controller/src/main/java/com/xin/tree/treeLevel.java
@@ -0,0 +1,14 @@
+package com.xin.tree;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class treeLevel {
+    public static void main(String[] args) {
+
+    }
+    private void printTree(){
+        Queue queue = new ArrayDeque();
+
+    }
+}
